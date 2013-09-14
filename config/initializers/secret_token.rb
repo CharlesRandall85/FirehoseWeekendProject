df859dd7822cc032dcf3c9f666f308ddfcdd6763
Charles <charles@charles-VirtualBox.(none)>
2013-09-14 16:13:41 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehose5::Application.config.secret_token = '6a99a6693e4b16671083e37634a842c821c7045ec10eb6c16fb930465ee06e7e8a73f5593513c2fa241f869f3aed275aa4dbac345649ab9c3a9cea075933ea01'
